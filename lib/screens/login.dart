1890515cf889f96c386625a3469ef6a44c06b9bd
karimwahba <karimwahba53@gmail.com>
2023-11-29 18:58:25 +0200

    newloginSignup

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -15,99 +15,231 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "EVENA",
-            style: TextStyle(color: Colors.white),
+        // appBar: AppBar(
+        //   title: const Text(
+        //     "EVENA",
+        //     style: TextStyle(color: Colors.white),
+        //   ),
+        //   backgroundColor: Colors.black,
+        // ),
+        body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: Image.asset('assets/images/bk.png').image,
+            ),
           ),
-          backgroundColor: Colors.black,
-        ),
-        body: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 50.0),
-              child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    fontSize: 35,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 150.0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 40,
+                      ),
+                    ),
                   ),
                 ),
-              ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            SizedBox(
-              width: 0.9 * MediaQuery.of(context).size.width,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Username',
-                  prefixIcon: Icon(Icons.person),
+                const SizedBox(
+                  height: 6,
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your username';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 0.9 * MediaQuery.of(context).size.width,
-              child: TextFormField(
-                decoration: const InputDecoration(
-                    labelText: 'Password',
-                    prefixIcon: Icon(Icons.lock),
-                    suffixIcon: Icon(Icons.remove_red_eye)),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Please enter your Password';
-                  }
-                  return null;
-                },
-              ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: 0.8 * MediaQuery.of(context).size.width,
-              height: 0.13 * MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                    builder: (c) {
-                      return UserHome();
-                    },
-                  ));
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
+                const Text(
+                  'Discover exciting events near you.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
                 ),
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+                const SizedBox(height: 200),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(1.0),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        width: 0.9 * MediaQuery.of(context).size.width,
+                        child: TextFormField(
+                          style: const TextStyle(color: Colors.black),
+                          decoration: const InputDecoration(
+                            labelText: 'Email',
+                            prefixIcon: Icon(
+                              Icons.mail,
+                              color: Colors.black,
+                            ),
+                            labelStyle: TextStyle(color: Colors.black),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                            hintStyle: TextStyle(color: Colors.black),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter your Email';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        width: 0.9 * MediaQuery.of(context).size.width,
+                        child: TextFormField(
+                          obscureText: true,
+                          style: const TextStyle(color: Colors.black),
+                          decoration: const InputDecoration(
+                            labelText: 'Password',
+                            prefixIcon: Icon(
+                              Icons.lock,
+                              color: Colors.black,
+                            ),
+                            suffixIcon: InkWell(
+                              child: Icon(
+                                Icons.remove_red_eye,
+                                color: Colors.black,
+                              ),
+                            ),
+                            labelStyle: TextStyle(color: Colors.black),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                            hintStyle: TextStyle(color: Colors.black),
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter your Password';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(height: 50),
+                      SizedBox(
+                        width: 0.8 * MediaQuery.of(context).size.width,
+                        height: 0.13 * MediaQuery.of(context).size.width,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (c) {
+                                return UserHome();
+                              },
+                            ));
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 255, 170, 0),
+                          ),
+                          child: const Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 255, 255, 255),
+                                fontSize: 25),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (c) {
+                              return Signup();
+                            },
+                          ));
+                        },
+                        child: const Text(
+                          'If you dont have an account? Sign up here!',
+                          style: TextStyle(color: Colors.black, fontSize: 12),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '-----------------------------',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'OR',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 15),
+                            ),
+                          ),
+                          Text(
+                            '-----------------------------',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white, // Button color
+                              onPrimary: Colors.black, // Text color
+                            ),
+                            child: const Row(
+                              children: [
+                                ImageIcon(
+                                  AssetImage('assets/images/googlelogo.png'),
+                                  size: 24,
+                                ),
+                                SizedBox(width: 10),
+                                Text('Google'),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(width: 20),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white, // Button color
+                              onPrimary: Colors.black, // Text color
+                            ),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.apple,
+                                  color: Colors.black,
+                                ),
+                                SizedBox(width: 10),
+                                Text('iOS'),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (c) {
-                    return Signup();
-                  },
-                ));
-              },
-              child: const Text(
-                'Don\'t have an account? Sign up here!',
-                style: TextStyle(color: Colors.black, fontSize: 12),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
