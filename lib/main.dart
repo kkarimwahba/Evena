6324b1cbdadf37c3f0a66d0ffe998c861cb3cdaa
kkarimwahba <karimwahba53@gmail.com>
2023-12-16 02:01:49 +0200

    firebase_config_1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
-import 'package:evena/screens/adminHome.dart';
-import 'package:evena/screens/admin_add.dart';
-import 'package:evena/screens/adminprofile.dart';
 import 'package:evena/screens/splashScreen.dart';
 import 'package:flutter/material.dart';
 
 main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home:SplashScreen() ,
+    home: SplashScreen(),
   ));
 }
