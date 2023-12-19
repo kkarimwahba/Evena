a32743c73f9c79958a6e420b8b4ac8814204fddc
MartinaRaafat77 <martina2009403@miuegypt.edu.eg>
2023-12-19 19:28:04 +0200

    .

--- lib/screens/Booking.dart
+++ lib/screens/Booking.dart
@@ -1,7 +1,7 @@
+import 'package:evena/screens/Payment.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'Payment.dart'; // Assuming you have a Payment page
 
 class Booking extends StatefulWidget {
   final List<int> selectedSeats;
