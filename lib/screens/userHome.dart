f70835ae5dd793d8974c9ec4eb33cd7067c7e8ad
kkarimwahba <karimwahba53@gmail.com>
2023-12-28 01:53:28 +0200

    some changes

--- lib/screens/userHome.dart
+++ lib/screens/userHome.dart
@@ -48,29 +48,52 @@ class _UserHomeState extends State<UserHome> {
     fetchEvents();
   }
 
+  bool loading = false;
+
   void fetchEvents() async {
     try {
+      // Set loading to true to indicate that events are being fetched
+      setState(() {
+        loading = true;
+      });
+
       final snapshot =
           await FirebaseFirestore.instance.collection('events').get();
 
+      DateTime currentDate = DateTime.now();
+
       setState(() {
-        allEvents = snapshot.docs.map((doc) {
+        // Initialize the lists with the correct type
+        allEvents = List<Event>.from(snapshot.docs.map((doc) {
           var event = doc.data() as Map<String, dynamic>;
-          return Event(
-            title: event['title'] ?? '',
-            description: event['description'] ?? '',
-            date: DateTime.parse(event['date'] ?? ''),
-            time: event['time'] ?? '',
-            location: event['location'] ?? '',
-            category: event['category'] ?? '',
-            price: event['price']?.toString() ?? '',
-            availability: event['availability']?.toString() ?? '',
-            imagePath: event['image'] ?? '', // Handle null or empty imagePath
-          );
-        }).toList();
+
+          // Check if the event date is in the future
+          final isFutureEvent =
+              DateTime.parse(event['date'] ?? '').isAfter(currentDate);
+
+          // Include the event only if it has a future date
+          if (isFutureEvent) {
+            return Event(
+              title: event['title'] ?? '',
+              description: event['description'] ?? '',
+              date: DateTime.parse(event['date'] ?? ''),
+              time: event['time'] ?? '',
+              location: event['location'] ?? '',
+              category: event['category'] ?? '',
+              price: event['price']?.toString() ?? '',
+              availability: event['availability']?.toString() ?? '',
+              imagePath: event['image'] ?? '',
+            );
+          }
+
+          return null; // Exclude events with past dates
+        }).where((event) => event != null));
 
         // Initialize the filteredEvents list with all events initially
-        filteredEvents = List.from(allEvents);
+        filteredEvents = List<Event>.from(allEvents);
+
+        // Set loading back to false now that events are fetched
+        loading = false;
       });
     } catch (error) {
       print('Error fetching events: $error');
@@ -79,13 +102,20 @@ class _UserHomeState extends State<UserHome> {
 
   void applyFilters(String searchValue) {
     print('Applying filters with: $searchValue');
+    DateTime currentDate = DateTime.now();
+
     setState(() {
       filteredEvents = allEvents.where((event) {
         final filterValue = _getFilterValue(event);
         final containsValue = filterValue
             .toLowerCase()
             .contains(searchValue.trim().toLowerCase());
-        return containsValue;
+
+        // Check if the event date is in the future
+        final isFutureEvent = event.date.isAfter(currentDate);
+
+        // Return true only if the event matches the search criteria and is in the future
+        return containsValue && isFutureEvent;
       }).toList();
     });
   }
@@ -124,124 +154,96 @@ class _UserHomeState extends State<UserHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: DrawerWidget(
-          title: 'Event Page',
-        ),
-        appBar: AppBar(
-          title: const Text('Event List'),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.filter_list),
-              onPressed: toggleFilter,
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextField(
-                      controller: _searchController,
-                      onChanged: (value) {
-                        // Apply filters when search bar changes
-                        applyFilters(value);
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        prefixIcon: const Icon(Icons.search),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
+      drawer: DrawerWidget(
+        title: 'Event Page',
+      ),
+      appBar: AppBar(
+        title: const Text('Event List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.filter_list),
+            onPressed: toggleFilter,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _searchController,
+                    onChanged: (value) {
+                      // Apply filters when search bar changes
+                      applyFilters(value);
+                    },
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                   ),
                 ),
-                if (_showFilter)
-                  DropdownButton<String>(
-                    value: _selectedFilter,
-                    items: [
-                      DropdownMenuItem(
-                        value: 'title',
-                        child: Text('Title'),
-                      ),
-                      DropdownMenuItem(
-                        value: 'location',
-                        child: Text('Location'),
-                      ),
-                      DropdownMenuItem(
-                        value: 'date',
-                        child: Text('Date'),
-                      ),
-                      DropdownMenuItem(
-                        value: 'category',
-                        child: Text('Category'),
-                      ),
-                    ],
-                    onChanged: (value) {
-                      setState(() {
-                        _selectedFilter = value!;
-                      });
-
-                      // Apply filters when the filter type changes
-                      applyFilters(_searchController.text);
-                    },
-                  ),
-              ],
-            ),
-            Expanded(
-              child: StreamBuilder<QuerySnapshot>(
-                stream:
-                    FirebaseFirestore.instance.collection('events').snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
-                  }
-
-                  if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  }
-
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return const Center(
-                      child: Text('No events available.'),
-                    );
-                  }
-
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      var event = snapshot.data!.docs[index].data()
-                          as Map<String, dynamic>;
-                      DateTime eventDate = DateTime.parse(event['date'] ?? '');
-
-                      // Check if the event date is in the future
-                      if (eventDate.isAfter(DateTime.now())) {
-                        return EventCard(
-                          imagePath: event['image'] ?? '',
-                          title: event['title'] ?? '',
-                          description: event['description'] ?? '',
-                          date: eventDate,
-                          time: event['time'] ?? '',
-                          location: event['location'] ?? '',
-                          category: event['category'] ?? '',
-                          price: event['price']?.toString() ?? '',
-                          availability: event['availability']?.toString() ?? '',
-                        );
-                      } else {
-                        // Event date is in the past, so don't display it
-                        return SizedBox
-                            .shrink(); // Empty SizedBox to make it invisible
-                      }
-                    },
-                  );
-                },
               ),
+              if (_showFilter)
+                DropdownButton<String>(
+                  value: _selectedFilter,
+                  items: [
+                    DropdownMenuItem(
+                      value: 'title',
+                      child: Text('Title'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'location',
+                      child: Text('Location'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'date',
+                      child: Text('Date'),
+                    ),
+                    DropdownMenuItem(
+                      value: 'category',
+                      child: Text('Category'),
+                    ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedFilter = value!;
+                    });
+
+                    // Apply filters when the filter type changes
+                    applyFilters(_searchController.text);
+                  },
+                ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredEvents.length,
+              itemBuilder: (context, index) {
+                var event = filteredEvents[index];
+
+                return EventCard(
+                  imagePath: event.imagePath,
+                  title: event.title,
+                  description: event.description,
+                  date: event.date,
+                  time: event.time,
+                  location: event.location,
+                  category: event.category,
+                  price: event.price,
+                  availability: event.availability,
+                );
+              },
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
 
